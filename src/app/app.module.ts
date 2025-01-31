66b866c5678fb0ab92045c944c998eb7c771301e
Joshua Bennett <bennettjoshua54@gmail.com>
2025-01-31 18:00:53 -0500

    Imports fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,9 @@ import { TableComponent } from './table/table.component';
 
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-// import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
-// import { MatLegacyCardModule as MatCardModule } from '@angular/material/legacy-card';
-// import { MatLegacyInputModule as MatInputModule } from '@angular/material/legacy-input';
 
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
 
 
 @NgModule({
@@ -24,9 +23,8 @@ import { FormsModule } from '@angular/forms';
     BrowserAnimationsModule,
     HttpClientModule,
     FormsModule,
-    // MatInputModule,
-    // MatButtonModule,
-    // MatCardModule,
+    MatInputModule,
+    MatCardModule,
     NoopAnimationsModule
   ],
   providers: [],
