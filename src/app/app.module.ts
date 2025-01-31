fa47ee96dcf43b068ba5ba37ac94c10fafbc5902
Joshua Bennett <bennettjoshua54@gmail.com>
2025-01-31 17:38:24 -0500

    To angular 15 part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,9 @@ import { TableComponent } from './table/table.component';
 
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatInputModule } from '@angular/material/input';
+import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
+import { MatLegacyCardModule as MatCardModule } from '@angular/material/legacy-card';
+import { MatLegacyInputModule as MatInputModule } from '@angular/material/legacy-input';
 
 
 
